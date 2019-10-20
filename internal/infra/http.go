185b4e7f20dc8bc0ddc99c1e609798c13d1ed356
Ramazan AYYILDIZ <rayyildiz@gmail.com>
2019-10-20 12:53:10 +0200

    add sentry log handler

--- internal/infra/http.go
+++ internal/infra/http.go
@@ -3,12 +3,19 @@ package infra
 import (
 	"time"
 
+	sentryHttp "github.com/getsentry/sentry-go/http"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
 func NewRouter() *chi.Mux {
 	r := chi.NewRouter()
+
+	logHandler := sentryHttp.New(sentryHttp.Options{
+		Repanic:         false,
+		WaitForDelivery: false,
+		Timeout:         5,
+	})
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
@@ -17,6 +24,7 @@ func NewRouter() *chi.Mux {
 	r.Use(middleware.Timeout(30 * time.Second))
 	r.Use(middleware.Heartbeat("/health"))
 	r.Use(middleware.Compress(6))
+	r.Use(logHandler.Handle)
 
 	return r
 }
